3c138c1ce02329177c6ef4ba539fba5c903bb59d
Daniel Naydenov <daninaydenow@gmail.com>
2021-12-07 11:14:44 +0200

    Create requester for fetching data

---
+++ src/api/requester.js
@@ -0,0 +1,26 @@
+const baseUrl = 'http://localhost:3030'
+
+export const requester = (endpoint, method, body, user) => {
+        if (method === undefined) {
+            method = 'Get';
+        }
+        let headers = {};
+        if (['post', 'put', 'patch'].includes(method.toLowerCase())) {
+            headers['Content-Type'] = 'application/json';
+        }
+        if(user) {
+            headers['X-Authorization'] = user.accessToken;
+        }
+        let options = {
+            headers,
+            method,
+        }
+        if (body !== undefined) {
+            options.body = JSON.stringify(body);
+        }
+        let resultUrl = baseUrl + endpoint;
+
+        return fetch(resultUrl, options);
+}
+
+    
