06d434022a52eab3973f101edc8f1085dae094f9
Daniel Naydenov <daninaydenow@gmail.com>
2021-12-09 14:45:22 +0200

    Add edit functionallity and service

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,23 +1,50 @@
+import { useEffect, useState, useContext } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import * as actorService from '../../services/actorService';
+import { AuthContext } from "../../contexts/AuthContext";
 import styles from './Edit.module.css'
 const Edit = () => {
+    const navigate = useNavigate();
+    const [actor, setActor] = useState({});
+    const {actorId} = useParams();
+    const {user} = useContext(AuthContext);
+
+    useEffect(() => {
+        actorService.getOne(actorId)
+        .then(res => {
+            console.log(res);
+            setActor(res);
+        });
+    }, [actorId]);
+
+    const editPortfolioHandler = (e) => {
+        e.preventDefault();
+        let {profImgUrl, name, genre, imgOneUrl, imgTwoUrl, imgThreeUrl, experience} = Object.fromEntries(new FormData(e.currentTarget));
+        actorService.update(user, actorId, {profImgUrl, name, genre, imgOneUrl, imgTwoUrl, imgThreeUrl, experience})
+        .then(res => {
+            console.log(res);
+            navigate(`/details/${actorId}`);
+        })
+    }
+
     return (
-        <form id="edit-form" method="POST">
+        <form id="edit-form" method="POST" onSubmit={editPortfolioHandler}>
             <div className={`card  ${styles.style}`}>
                 <div className="row h-100 text-start">
                     <div className={`col-3 ps-3 pe-3`}>
                         <div className={`h-100`}>
                             <div className={` ${styles.border} ${styles.imgsize} text-center mb-2`}>
                                 <h2 className="mb-5">Edit Portfolio</h2>
-                                <label htmlFor="profileImg" className="mt-4"> Profile Image Url</label>
-                                <input type="text" name="profileImg" className="form-control" />
+                                <label htmlFor="profImgUrl" className="mt-4"> Profile Image Url</label>
+                                <input type="text" name="profImgUrl" className="form-control" defaultValue={actor.profImgUrl}/>
                             </div>
                             <div className={` ${styles.border} ${styles.general} ps-2 pt-2 mb-2`}>
                                 <label htmlFor="name" className="ps-2 pe-4 mt-1">Name</label>
-                                <input type="text" name="name" className="form-control" />
+                                <input type="text" name="name" className="form-control" defaultValue={actor.name}/>
                             </div>
                             <div className={` ${styles.border} ${styles.general} ps-2 pt-2 mb-2`}>
                                 <label htmlFor="genre" className="ps-2 pe-4 mt-1">Genre</label>
-                                <input type="text" name="genre" className="form-control" />
+                                <input type="text" name="genre" className="form-control" defaultValue={actor.genre}/>
                             </div>
                         </div>
                     </div>
@@ -25,21 +52,21 @@ const Edit = () => {
                         <div className={`h-100`}>
                             <div className={` ${styles.border}  ps-2 pt-2 h-50 mb-2`}>
                                 <div className={`${styles.imgUrl}`}>
-                                    <label htmlFor="imgOne">Image 1 Url</label>
-                                    <input type="text" name="imgOne" className="form-control" />
+                                    <label htmlFor="imgOneUrl">Image 1 Url</label>
+                                    <input type="text" name="imgOneUrl" className="form-control" defaultValue={actor.imgOneUrl} />
                                 </div>
                                 <div className={`${styles.imgUrl} mt-3`}>
-                                    <label htmlFor="imgTwo">Image 2 Url</label>
-                                    <input type="text" name="imgTwo" className="form-control" />
+                                    <label htmlFor="imgTwoUrl">Image 2 Url</label>
+                                    <input type="text" name="imgTwoUrl" className="form-control" defaultValue={actor.imgTwoUrl}/>
                                 </div>
                                 <div className={`${styles.imgUrl} mt-3`}>
-                                    <label htmlFor="imgThree">Image 3 Url</label>
-                                    <input type="text" name="imgThree" className="form-control" />
+                                    <label htmlFor="imgThreeUrl">Image 3 Url</label>
+                                    <input type="text" name="imgThreeUrl" className="form-control" defaultValue={actor.imgThreeUrl} />
                                 </div>
                             </div>
                             <div className={` ${styles.border} ps-2 pt-2 h-25 mb-2`}>
                                 <h4>Experience:</h4>
-                                <textarea className="form-control" rows="5"></textarea>
+                                <textarea className="form-control" rows="5" name="experience" defaultValue={actor.experience} />
                             </div>
                             <div className="text-center">
                                 <button className={'btn btn-warning mt-5'}>Edit Portfolio</button>
--- src/services/actorService.js
+++ src/services/actorService.js
@@ -5,6 +5,10 @@ const endPoint = '/data/actors';
 export const create = (actorData, user) => {
     return requester(endPoint, "POST", actorData, user)
 }
+export const update = (user, actorId, actorData) => {
+    const updateEndPoint = `${endPoint}/${actorId}`;
+    return requester(updateEndPoint, "PUT", actorData, user);
+} 
 
 export const getAll = () => {
     return requester(endPoint, "GET", undefined, undefined)
