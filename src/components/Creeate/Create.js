d848caa9c1118a8a651f9b72a9d003c1582b1b4e
Daniel Naydenov <daninaydenow@gmail.com>
2021-12-09 14:45:05 +0200

    Put placeholders for all fiedls in create form

--- src/components/Creeate/Create.js
+++ src/components/Creeate/Create.js
@@ -7,7 +7,7 @@ const Create = () => {
     const navigate = useNavigate();
     const {user} = useContext(AuthContext);
     
-    const createHandler = (e) => {
+    const createPortfolioHandler = (e) => {
         e.preventDefault();
         const {profImgUrl, name, genre, imgOneUrl, imgTwoUrl, imgThreeUrl, experience} = Object.fromEntries(new FormData(e.currentTarget));
         // TODO: validate fields
@@ -19,7 +19,7 @@ const Create = () => {
     }
 
     return (
-        <form method="POST" onSubmit={createHandler}>
+        <form method="POST" onSubmit={createPortfolioHandler}>
             <div className={`card  ${styles.style}`}>
                 <div className="row h-100 text-start">
                     <div className={`col-3 ps-3 pe-3`}>
@@ -27,15 +27,15 @@ const Create = () => {
                             <div className={` ${styles.border} ${styles.imgsize} text-center mb-2`}>
                                 <h2 className="mb-5">Create Portfolio</h2>
                                 <label htmlFor="profImgUrl" className="mt-4"> Profile Image Url</label>
-                                <input type="text" name="profImgUrl" className="form-control" />
+                                <input type="text" name="profImgUrl" className="form-control" placeholder="Enter the url of Your profile Image" />
                             </div>
                             <div className={` ${styles.border} ${styles.general} ps-2 pt-2 mb-2`}>
                                 <label htmlFor="name" className="ps-2 pe-4 mt-1">Name</label>
-                                <input type="text" name="name" className="form-control" />
+                                <input type="text" name="name" className="form-control" placeholder="Enter your full name" />
                             </div>
                             <div className={` ${styles.border} ${styles.general} ps-2 pt-2 mb-2`}>
                                 <label htmlFor="genre" className="ps-2 pe-4 mt-1">Genre</label>
-                                <input type="text" name="genre" className="form-control" />
+                                <input type="text" name="genre" className="form-control" placeholder="Enter all specific genres You act in" />
                             </div>
                         </div>
                     </div>
@@ -44,20 +44,20 @@ const Create = () => {
                             <div className={` ${styles.border}  ps-2 pt-2 h-50 mb-2`}>
                                 <div className={`${styles.imgUrl}`}>
                                     <label htmlFor="imgOneUrl">Image 1 Url</label>
-                                    <input type="text" name="imgOneUrl" className="form-control" />
+                                    <input type="text" name="imgOneUrl" className="form-control" placeholder="Enter the url of Your portfolio Image" />
                                 </div>
                                 <div className={`${styles.imgUrl} mt-3`}>
                                     <label htmlFor="imgTwoUrl">Image 2 Url</label>
-                                    <input type="text" name="imgTwoUrl" className="form-control" />
+                                    <input type="text" name="imgTwoUrl" className="form-control" placeholder="Enter the url of Your portfolio Image" />
                                 </div>
                                 <div className={`${styles.imgUrl} mt-3`}>
                                     <label htmlFor="imgThreeUrl">Image 3 Url</label>
-                                    <input type="text" name="imgThreeUrl" className="form-control" />
+                                    <input type="text" name="imgThreeUrl" className="form-control" placeholder="Enter the url of Your portfolio Image"/>
                                 </div>
                             </div>
                             <div className={` ${styles.border} ps-2 pt-2 h-25 mb-2`}>
                                 <h4>Experience:</h4>
-                                <textarea className="form-control" rows="5" name="experience"></textarea>
+                                <textarea className="form-control" rows="5" name="experience" placeholder="Enter Your previous theater and cinema experience"></textarea>
                             </div>
                             <div className="text-center">
                                 <button className={'btn btn-warning mt-5'}>Create Portfolio</button>
