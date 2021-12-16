c4ec8414828cb0c2f62e31b75c9b11a352d0a8aa
Daniel Naydenov <daninaydenow@gmail.com>
2021-12-16 15:23:41 +0200

    Add form validation to Create

--- src/components/Creeate/Create.js
+++ src/components/Creeate/Create.js
@@ -1,19 +1,63 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
-import {  useAuth } from '../../contexts/AuthContext';
+import { useAuth } from '../../contexts/AuthContext';
 import * as actorService from '../../services/actorService';
 import styles from './Create.module.css'
+
+const initialFormValues = {
+    profImgUrl: '',
+    name: '',
+    genre: '',
+    imgOneUrl: '',
+    imgTwoUrl: '',
+    imgThreeUrl: '',
+    experience: '',
+}
+
 const Create = () => {
     const navigate = useNavigate();
-    const {currentUser} = useAuth();
-    
+    const { currentUser } = useAuth();
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    useEffect(() => {
+        if(Object.keys(formErrors).length === 0 && isSubmit) {
+            actorService.create({...formValues, likes:[], _ownerId: currentUser.uid })
+            .then(res => {
+               navigate('/portfolios');
+            })
+        }
+        
+    }, [formErrors, isSubmit]);
+
     const createPortfolioHandler = (e) => {
-        e.preventDefault(); 
-        const {profImgUrl, name, genre, imgOneUrl, imgTwoUrl, imgThreeUrl, experience} = Object.fromEntries(new FormData(e.currentTarget));
-        // TODO: validate fields
-        actorService.create({profImgUrl, name, genre, imgOneUrl, imgTwoUrl, imgThreeUrl, experience, likes:[], _ownerId: currentUser.uid })
-        .then(res => {
-           navigate('/portfolios');
-        })
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
+    }
+
+    const onChangeHandler = (e) => {
+        console.log(formValues);
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    }
+
+    const validate = (values) => {
+        const errors = {};
+        if (!values.profImgUrl) {
+            errors.profImgUrl = "Profile image URL is required!"
+        }
+        if (!values.name) {
+            errors.name = "Actor name is required!"
+        }
+        if (!values.genre) {
+            errors.genre = "Genre is required!"
+        }
+        if (!values.experience) {
+            errors.experience = "Actor experience is required!"
+        }
+        return errors;
     }
 
     return (
@@ -24,16 +68,40 @@ const Create = () => {
                         <div className={`h-100`}>
                             <div className={` ${styles.border} ${styles.imgsize} text-center mb-2`}>
                                 <h2 className="mb-5">Create Portfolio</h2>
-                                <label htmlFor="profImgUrl" className="mt-4"> Profile Image Url</label>
-                                <input type="text" name="profImgUrl" className="form-control" placeholder="Enter the url of Your profile Image" />
+                                <label htmlFor="profImgUrl" className="mt-4"> Profile Image Url <span className='text-danger'>*</span></label>
+                                <input
+                                    type="text"
+                                    name="profImgUrl"
+                                    className="form-control"
+                                    placeholder="Enter the url of Your profile Image"
+                                    value={formValues.profImgUrl}
+                                    onChange={onChangeHandler}
+                                />
+                                <span className='text-danger'>{formErrors.profImgUrl}</span>
                             </div>
                             <div className={` ${styles.border} ${styles.general} ps-2 pt-2 mb-2`}>
-                                <label htmlFor="name" className="ps-2 pe-4 mt-1">Name</label>
-                                <input type="text" name="name" className="form-control" placeholder="Enter your full name" />
+                                <label htmlFor="name" className="ps-2 pe-4 mt-1">Name <span className='text-danger'>*</span></label>
+                                <input 
+                                type="text" 
+                                name="name" 
+                                className="form-control" 
+                                placeholder="Enter your full name" 
+                                value={formValues.name}
+                                onChange={onChangeHandler}
+                                />
+                                <span className='text-danger'>{formErrors.name}</span>
                             </div>
                             <div className={` ${styles.border} ${styles.general} ps-2 pt-2 mb-2`}>
-                                <label htmlFor="genre" className="ps-2 pe-4 mt-1">Genre</label>
-                                <input type="text" name="genre" className="form-control" placeholder="Enter all specific genres You act in" />
+                                <label htmlFor="genre" className="ps-2 pe-4 mt-1">Genre <span className='text-danger'>*</span></label>
+                                <input 
+                                type="text" 
+                                name="genre" 
+                                className="form-control" 
+                                placeholder="Enter all specific genres You act in" 
+                                value={formValues.genre}
+                                onChange={onChangeHandler}
+                                />
+                                <span className='text-danger'>{formErrors.genre}</span>
                             </div>
                         </div>
                     </div>
@@ -50,12 +118,19 @@ const Create = () => {
                                 </div>
                                 <div className={`${styles.imgUrl} mt-3`}>
                                     <label htmlFor="imgThreeUrl">Image 3 Url</label>
-                                    <input type="text" name="imgThreeUrl" className="form-control" placeholder="Enter the url of Your portfolio Image"/>
+                                    <input type="text" name="imgThreeUrl" className="form-control" placeholder="Enter the url of Your portfolio Image" />
                                 </div>
                             </div>
                             <div className={` ${styles.border} ps-2 pt-2 h-25 mb-2`}>
-                                <h4>Experience:</h4>
-                                <textarea className="form-control" rows="5" name="experience" placeholder="Enter Your previous theater and cinema experience"></textarea>
+                                <h4>Experience: <span className='text-danger'>*</span></h4>
+                                <textarea 
+                                className="form-control" 
+                                rows="5" name="experience" 
+                                placeholder="Enter Your previous theater and cinema experience" 
+                                value={formValues.experience}
+                                onChange={onChangeHandler}
+                                />
+                                <span className='text-danger'>{formErrors.experience}</span>
                             </div>
                             <div className="text-center">
                                 <button className={'btn btn-warning mt-5'}>Create Portfolio</button>
