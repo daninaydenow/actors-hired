12562df238685f55dff6c0691dbd2b8d49e01cfc
Daniel Naydenov <daninaydenow@gmail.com>
2021-12-17 16:32:54 +0200

    Update load spinner's colors

--- src/helpers/loadSpinner.js
+++ src/helpers/loadSpinner.js
@@ -1,10 +1,10 @@
 export const loadSpinner = (
     <>
-      <div className="spinner-grow text-success" role="status">
-      </div>
       <div className="spinner-grow text-warning" role="status">
       </div>
       <div className="spinner-grow text-danger" role="status">
+      </div>
+      <div className="spinner-grow text-warning" role="status">
       </div>
       <div className="text-light ms-2">Loading....</div>
     </>
