f989b3366817ddc1b57e61a3d64e62e1fb47dd09
Daniel Naydenov <daninaydenow@gmail.com>
2021-12-17 15:12:55 +0200

    Add loading spinner to portfolios

--- src/components/Portfolios/Portfolios.js
+++ src/components/Portfolios/Portfolios.js
@@ -2,26 +2,31 @@ import { useState, useEffect } from "react";
 import PortfolioCard from "./PortfolioCard";
 
 import * as actorService from '../../services/actorService';
+import {loadSpinner} from '../../helpers/loadSpinner';
 import styles from './Portfolios.module.css';
 const Portfolios = () => {
-     const [portfolios, setPortfolios] = useState([]);
+  const [portfolios, setPortfolios] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      actorService.getAll()
+        .then(mySnapShot => {
+          setPortfolios(mySnapShot.docs.map((doc) => ({ ...doc.data(), _id: doc.id })))
+          setLoading(false);
+        })
+    }, 3000);
+  }, []);
 
-     useEffect(() => {
-      
-           actorService.getAll()
-           .then(mySnapShot => {
-            setPortfolios(mySnapShot.docs.map((doc) => ({...doc.data(), _id: doc.id})))
-            
-           }) 
-     }, []);
-     
+
+
   return (
     <>
       <h1 className={styles.heading}>Browse Portfolios</h1>
       <div className={styles.flex}>
-        { portfolios
-        ? portfolios.map(x => <PortfolioCard key={x._id} {...x} />)
-        : <h1>No portfolios have been created yet!</h1>}
+        {loading && loadSpinner}
+        {portfolios
+          ? portfolios.map(x => <PortfolioCard key={x._id} {...x} />)
+          : <h1>No portfolios have been created yet!</h1>}
       </div>
     </>
 
